3750eb8a67e90a9f76132dfd881715637df8244a
sleepyknife <sleepycat7912@gmail.com>
2025-05-12 23:24:22 +0800

    use API KEY

--- utils/sheets.js
+++ utils/sheets.js
@@ -1,21 +1,15 @@
-import { google } from 'googleapis';
-
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    client_email: process.env.GOOGLE_CLIENT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-  },
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
-
 const SHEET_ID = process.env.SHEET_ID;
+const API_KEY = process.env.GOOGLE_API_KEY;
 const SHEET_NAME = 'Reponse';
 
 export async function getSheetData() {
-  const sheets = google.sheets({ version: 'v4', auth: await auth.getClient() });
-  const res = await sheets.spreadsheets.values.get({
-    spreadsheetId: SHEET_ID,
-    range: SHEET_NAME,
-  });
-  return res.data;
+  const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${encodeURIComponent(SHEET_NAME)}?key=${API_KEY}`;
+
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`Google Sheets API 錯誤：${res.status}`);
+  }
+
+  const data = await res.json();
+  return data;
 }
