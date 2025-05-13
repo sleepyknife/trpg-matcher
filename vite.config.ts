e1833f10df7b7c10ad66c68b5c0dacf3217cd9b7
sleepyknife <sleepycat7912@gmail.com>
2025-05-13 23:54:14 +0800

    Revert "add card"
    This reverts commit 5b76c17e5a649cb89afd87c3407eb43b278542b0.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite'
-
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [vue(), tailwindcss()],
+    plugins: [vue()],
     base: env.VITE_APP_BASE_URL || '/'
   };
 });
